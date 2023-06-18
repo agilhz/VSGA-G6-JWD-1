7a878e4cbd9b4d658cf1ccdfa8b03929ef714ba3
agilhz <agilhz15@gmail.com>
2023-06-18 12:52:07 +0700

    commit untuk exercise Hari_5

---
+++ Hari_5/kalkulator.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Kalkulator</h1>
+    <form action="" method="POST">
+      <input type="number" name="a" placeholder="Bilangan a" />
+
+      <select name="operator">
+        <option selected disabled>Pilih Operator</option>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+
+      <input type="number" name="b" placeholder="Bilangan b" />
+
+      <div style="margin-top: 1rem">
+        <button type="button" onclick="location.href = '?clear'">Clear</button>
+        <button type="submit">Hitung</button>
+      </div>
+    </form>
+    <?php
+    if (!empty($_POST)):
+        $a = (double) @$_POST['a'];
+        $b = (double) @$_POST['b'];
+        $operator = @$_POST['operator'];
+        
+        switch ($operator) {
+            case '+':
+                $hasil = $a + $b;
+                break;
+            case '-':
+                $hasil = $a - $b;
+                break;
+            case '*':
+                $hasil = $a * $b;
+                break;
+            case '/':
+                $hasil = $a / $b;
+                break;
+        }
+        ?>
+      <p>Hasil: <?php echo $hasil; ?></p>
+    <?php endif; ?>
+  </body>
+</html>
